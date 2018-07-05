786e4ba518a938bc31d3040411ee6a94f0825648
hershyd <hershyd>
2018-07-05 22:17:48 +0300

    Changed connection string

--- ShukRouting/ShukRouting.DataAccess/DataSource/ShukRoutingContext.cs
+++ ShukRouting/ShukRouting.DataAccess/DataSource/ShukRoutingContext.cs
@@ -1,17 +1,11 @@
 ﻿using ShukRouting.DataAccess.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ShukRouting.DataAccess.Models;
 
 namespace ShukRouting.DataAccess.DataSource
 {
-   public class ShukRoutingContext: DbContext
+    public class ShukRoutingContext : DbContext
     {
-        
+
         public DbSet<Commodity> Commodities { get; set; }
         public DbSet<Stall> Stalls { get; set; }
         public DbSet<CommodityStall> CommoditiesStalls { get; set; }
