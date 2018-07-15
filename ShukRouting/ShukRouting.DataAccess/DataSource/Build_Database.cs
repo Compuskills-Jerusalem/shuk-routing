2ed2997c2c50897f1b2a69058e8650dc260075c3
hershyd <hershy9302@gmail.com>
2018-07-15 21:16:40 +0300

    fixed web.config

--- ShukRouting/ShukRouting.DataAccess/DataSource/Build_Database.cs
+++ ShukRouting/ShukRouting.DataAccess/DataSource/Build_Database.cs
@@ -19,9 +19,9 @@ namespace shuk_routing
                 string drop_stalls_table = "DROP TABLE Stalls;";
                 string drop_commodities_stalls_table = "DROP TABLE CommoditiesStalls;";
 
-                string define_commodities_table = "CREATE dbo.TABLE Commodities(CommodityID INT NOT NULL PRIMARY KEY IDENTITY(1, 1), CommodityName NVARCHAR(30) NOT NULL);";
+                string define_commodities_table = "CREATE TABLE dbo.Commodities(CommodityID INT NOT NULL PRIMARY KEY IDENTITY(1, 1), CommodityName NVARCHAR(30) NOT NULL);";
                 string define_stalls_table = "CREATE TABLE dbo.Stalls(StallID INT NOT NULL PRIMARY KEY IDENTITY(1, 1), StallName NVARCHAR(50) NOT NULL, FirstCoord INT, SecondCoord INT); ";
-                string define_commodities_stalls_table = "CREATE dbo.TABLE CommoditiesStalls(CommoditiesStallsID INT NOT NULL PRIMARY KEY IDENTITY(1, 1), CommoditiesID INT NOT NULL FOREIGN KEY REFERENCES Commodities(Commodity_ID), StallsID INT NOT NULL FOREIGN KEY REFERENCES Stalls(Stall_ID), Price SMALLMONEY NOT NULL CHECK(Price > 0), Rating INT CHECK(Rating >= 1 AND Rating <= 5), TimeRegistered DateTime NOT NULL DEFAULT(SYSDATETIME()), Notes NVARCHAR(140));";
+                string define_commodities_stalls_table = "CREATE TABLE dbo.CommoditiesStalls(CommoditiesStallsID INT NOT NULL PRIMARY KEY IDENTITY(1, 1), CommoditiesID INT NOT NULL FOREIGN KEY REFERENCES Commodities(Commodity_ID), StallsID INT NOT NULL FOREIGN KEY REFERENCES Stalls(Stall_ID), Price SMALLMONEY NOT NULL CHECK(Price > 0), Rating INT CHECK(Rating >= 1 AND Rating <= 5), TimeRegistered DateTime NOT NULL DEFAULT(SYSDATETIME()), Notes NVARCHAR(140));";
 
                 string data_for_commodities_table = "INSERT INTO Commodities (CommodityName) VALUES('Grapes'), ('Bananas'), ('French Rolls');";
                 string data_for_stalls_table = "INSERT INTO Stalls(StallName, FirstCoord, SecondCoord) Values ('Chaim''s Stall', 32, 40), ('Roland''s Rolls', 50, 19), ('Shabbos Treats', 12, 31);";
