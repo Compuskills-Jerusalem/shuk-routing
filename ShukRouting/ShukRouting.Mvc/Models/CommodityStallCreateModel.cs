702b67031ca0866629b896b4eec25a479e46038f
hershyd <hershy9302@gmail.com>
2018-07-17 16:14:30 +0300

    added new Commodity Stall CreateModel

---
+++ ShukRouting/ShukRouting.Mvc/Models/CommodityStallCreateModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShukRouting.Mvc.Models
+{
+    public class CommodityStallCreateModel
+    {
+        [Display(Name = "Commodity Stall ID")]
+        public int CommodityStallID { get; set; }
+
+        public int CommodityID { get; set; }
+        public string CommodityName { get; set; }
+
+        public int StallID { get; set; }
+        public string StallName { get; set; }
+        public IEnumerable<SelectListItem> StallNames { get; set; }
+
+        public Decimal Price { get; set; }
+        public int? Rating { get; set; }
+        public DateTime TimeRegistered { get; set; }
+        public string Notes { get; set; }
+    }
+}
