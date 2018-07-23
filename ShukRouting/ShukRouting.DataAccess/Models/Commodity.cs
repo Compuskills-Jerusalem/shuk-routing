55c8c241468d387845f1c7e7d68f578439f54997
ShmielB <shmilybrody@gmail.com>
2018-07-23 18:43:48 +0300

    Commodity Repo

--- ShukRouting/ShukRouting.DataAccess/Models/Commodity.cs
+++ ShukRouting/ShukRouting.DataAccess/Models/Commodity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace ShukRouting.DataAccess.Models
 {
     public class Commodity
     {
+        [Display(Name = "Commodity ID")]
         public int CommodityID { get; set; }
+
+        [Display(Name = "Commodity Name")]
         public string CommodityName { get; set; }
     }
 }
---
+++ ShukRouting/ShukRouting.Mvc/Data/CommodityRepository.cs
@@ -0,0 +1,35 @@
+﻿using ShukRouting.DataAccess.DataSource;
+using ShukRouting.DataAccess.Models;
+using ShukRouting.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShukRouting.Mvc.Data
+{
+    public class CommodityRepository
+    {
+        public List<CommodityModel> GetCommodities()
+        {
+            List<CommodityModel> CommoditiesDisplay = new List<CommodityModel>();
+            using (var ctx = new ShukRoutingContext())
+            {
+                var commodities = ctx.Commodities.AsNoTracking()
+                                .ToList();
+
+                foreach (var item in commodities)
+                {
+                    var comm = new CommodityModel
+                    {
+                        CommodityID = item.CommodityID,
+                        CommodityName = item.CommodityName
+                    };
+                    CommoditiesDisplay.Add(comm);
+                }
+
+            }
+            return CommoditiesDisplay;
+        }
+    }
+}
